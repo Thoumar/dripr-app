e35411e0da774b71cd9d238a1df59e5f2674e098
Thoumar <thomas@thoumar.com>
2020-12-05 12:33:38 +0100

    Mocking up layout

--- app/src/main/java/com/dripr/dripr/activities/PlaceActivity.kt
+++ app/src/main/java/com/dripr/dripr/activities/PlaceActivity.kt
@@ -1,6 +1,9 @@
 package com.dripr.dripr.activities
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.dripr.dripr.R
@@ -18,6 +21,30 @@ class PlaceActivity : AppCompatActivity() {
 
         place = intent.getParcelableExtra("Place")
 
+        placeName.text = place?.name
+        placeAddress.text = place?.address
+
+
+        addressContainer.setOnClickListener {
+            val intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("geo:0,0?q=${place?.latitude},${place?.longitude} (" + place?.name + ")")
+            )
+            intent.setPackage("com.google.android.apps.maps")
+            startActivity(intent)
+        }
+        placeShort.loadData(
+            "<html><body>" + place?.short + "</body></html>",
+            "text/html; charset=utf-8",
+            "utf-8"
+        )
+        placeDescription.loadData(
+            "<html><body>" + place?.description + "</body></html>",
+            "text/html; charset=utf-8",
+            "utf-8"
+        )
+        placeWebsite.loadUrl(place!!.website)
+
         carouselView.pageCount = place?.pictures!!.size
         carouselView.setImageListener(imageListener)
     }
@@ -28,5 +55,12 @@ class PlaceActivity : AppCompatActivity() {
             .with(baseContext)
             .load(place?.pictures!![position]["src"])
             .into(imageView)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+        }
+        return true
     }
 }
