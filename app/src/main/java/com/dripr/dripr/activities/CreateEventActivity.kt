a94339e0a951b27591c4fb0f8ef5940cf8789dfc
Thoumar <thomas@thoumar.com>
2020-12-06 23:47:05 +0100

    Adding date to events database

---
+++ app/src/main/java/com/dripr/dripr/activities/CreateEventActivity.kt
@@ -0,0 +1,88 @@
+package com.dripr.dripr.activities
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.dripr.dripr.R
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog
+import kotlinx.android.synthetic.main.activity_create_event.*
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+
+class CreateEventActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener,
+        TimePickerDialog.OnTimeSetListener {
+
+    val TAG = "[CreateEventActivity]"
+    var eventDate: String? = null
+    var eventTime: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_event)
+
+        dateTextView.setOnClickListener {
+            val now = Calendar.getInstance()
+            val dpd: DatePickerDialog = DatePickerDialog.newInstance(
+                    this@CreateEventActivity,
+                    now[Calendar.YEAR],
+                    now[Calendar.MONTH],
+                    now[Calendar.DAY_OF_MONTH]
+            )
+
+            dpd.show(supportFragmentManager, "ok")
+        }
+
+        timeTextView.setOnClickListener {
+            val tpd: TimePickerDialog? = TimePickerDialog.newInstance(
+                    this@CreateEventActivity, true
+            )
+
+            tpd?.show(supportFragmentManager, "ok")
+        }
+
+        createEvent.setOnClickListener {
+            val event_name = eventName.text.toString()
+            val DEFAULT_PATTERN = "yyyy-MM-dd HH:mm:ss"
+
+            val formatter: DateFormat = SimpleDateFormat(DEFAULT_PATTERN)
+            val event_date = formatter.parse("${eventDate} ${eventTime}")
+
+            val newEventData = hashMapOf(
+                    "name" to event_name,
+                    "date" to event_date,
+                    "ownerId" to Firebase.auth.uid
+            )
+
+            FirebaseFirestore.getInstance().collection("events")
+                    .add(newEventData)
+                    .addOnSuccessListener {
+                        Toast.makeText(
+                                baseContext,
+                                "DocumentSnapshot successfully written!",
+                                Toast.LENGTH_LONG
+                        ).show()
+                        Log.d(TAG, "DocumentSnapshot successfully written!")
+                        finish()
+                    }.addOnFailureListener { e ->
+                        Toast.makeText(baseContext, "Error writing document", Toast.LENGTH_LONG).show()
+                        Log.w(TAG, "Error writing document", e)
+                    }
+        }
+    }
+
+    override fun onDateSet(view: DatePickerDialog?, year: Int, monthOfYear: Int, dayOfMonth: Int) {
+        eventDate = "" + year + "-" + (monthOfYear + 1) + "-" + dayOfMonth
+        dateTextView.text = eventDate
+    }
+
+    override fun onTimeSet(view: TimePickerDialog?, hourOfDay: Int, minute: Int, second: Int) {
+        eventTime = "$hourOfDay:$minute:$second"
+        timeTextView.text = eventTime
+    }
+}
--- app/src/main/java/com/dripr/dripr/fragments/main/EventsFragment.kt
+++ app/src/main/java/com/dripr/dripr/fragments/main/EventsFragment.kt
@@ -1,21 +1,23 @@
 package com.dripr.dripr.fragments.main
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.dripr.dripr.R
+import com.dripr.dripr.activities.CreateEventActivity
+import kotlinx.android.synthetic.main.fragment_events.view.*
 
 class EventsFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v: View = inflater.inflate(R.layout.fragment_events, container, false)
 
-
+        v.createEventBtn.setOnClickListener {
+            val i = Intent(this.activity, CreateEventActivity::class.java)
+            startActivity(i)
+        }
 
         return v
     }
