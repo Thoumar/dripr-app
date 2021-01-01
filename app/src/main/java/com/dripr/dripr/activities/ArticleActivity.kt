32a9076920c8c0799cbd1b57f9575878b61887a6
Thoumar <thomas@thoumar.com>
2021-01-02 00:33:17 +0100

    Passing article data to ArticleActivity

--- app/src/main/java/com/dripr/dripr/activities/ArticleActivity.kt
+++ app/src/main/java/com/dripr/dripr/activities/ArticleActivity.kt
@@ -3,10 +3,13 @@ package com.dripr.dripr.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.dripr.dripr.R
+import com.dripr.dripr.entities.Article
 
 class ArticleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article)
+
+        val article = intent.getParcelableExtra<Article>("ARTICLE")
     }
 }
